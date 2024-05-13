d2f8b66d3f7711c530ce8138bfae8782ccf639cd
tungbeoooo23 <estebanh35pun0suy@hotmail.com>
2024-05-13 07:23:24 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-function numIslands(grid) {
-  if (grid.length === 0) return 0;
-  let count = 0;
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === "1") {
-        dfs(grid, i, j);
-        count++;
-      }
-    }
-  }
-  return count;
-}
-function dfs(grid, i, j) {
-  if (
-    i < 0 ||
-    i >= grid.length ||
-    j < 0 ||
-    j >= grid[0].length ||
-    grid[i][j] === "0"
-  )
-    return;
-  grid[i][j] = "0";
-  dfs(grid, i + 1, j);
-  dfs(grid, i - 1, j);
-  dfs(grid, i, j + 1);
-  dfs(grid, i, j - 1);
-}
+const originalObject = { name: "Alice", age: 30 };
+const clonedObject = JSON.parse(JSON.stringify(originalObject));
+console.log(clonedObject); // { name: "Alice", age: 30 }
